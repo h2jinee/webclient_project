65677c65fe1917ae00f906b5870cfbd1e95f6153
h2jinee <wjsgmlwls97@gmail.com>
2019-11-15 19:03:28 +0900

    commit

--- js/interest.js
+++ js/interest.js
@@ -59,3 +59,12 @@ $( document ).ready(function() {
  });
 });
 
+$(function() {
+    $('ul.tab li').click(function() {
+        var activeTab = $(this).attr('data-tab');
+        $('ul.tab li').removeClass('current');
+        $('.tabcontent').removeClass('current');
+        $(this).addClass('current');
+        $('#' + activeTab).addClass('current');
+    })
+});
